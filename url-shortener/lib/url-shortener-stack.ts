de81a8f1507b73d070a88a562fa64c6c981a4cc5
Darren Holland <darrenh@gridconnect.com>
2020-01-12 14:22:35 -0600

    Provide domain to construct

--- url-shortener/lib/url-shortener-stack.ts
+++ url-shortener/lib/url-shortener-stack.ts
@@ -3,8 +3,12 @@ import s3 = require('@aws-cdk/aws-s3');
 import lambda = require('@aws-cdk/aws-lambda');
 import apigw = require('@aws-cdk/aws-apigateway');
 
+interface UrlShortenerProps extends cdk.StackProps {
+  domain?: string
+}
+
 export class UrlShortenerStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props?: UrlShortenerProps) {
     super(scope, id, props);
 
     const bucket = new s3.Bucket(this, 'UrlShortnerBucket', {
@@ -13,7 +17,12 @@ export class UrlShortenerStack extends cdk.Stack {
       websiteErrorDocument: 'error.html'
     });
 
+    let domain = `http://${bucket.bucketWebsiteDomainName}`
+
     const code = lambda.Code.fromAsset('./dist/index.zip');
+    if (props && props.domain) {
+      domain = props.domain
+    }
 
     const fn = new lambda.Function(this, 'UrlShortenerFunction', {
       runtime: lambda.Runtime.NODEJS_12_X,
@@ -21,7 +30,7 @@ export class UrlShortenerStack extends cdk.Stack {
       code,
       environment: {
         BUCKET_NAME: bucket.bucketName,
-        DOMAIN: `http://${bucket.bucketWebsiteDomainName}`
+        DOMAIN: domain
       }
     });
 
